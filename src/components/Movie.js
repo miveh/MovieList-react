508526636eda416168eb1dd2c05773435cddcca5
miveh <miveh@imivechi@gmail.com>
2023-10-22 23:02:41 +0330

    movie component add

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Movie({ id, name, source, cover }) {
-  return <div className="Movie"></div>;
+  return (
+    <div className="Movie">
+      <div className="bg-purple-300 Cover">
+        <img className="" src={cover} />
+      </div>
+      <div className="Content">
+        <p>{name}</p>
+        <div className="StarRating">star</div>
+        <div>
+          <div className="Watched">watched</div>
+          <img src="./images/deleteBtn.svg" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Movie;
